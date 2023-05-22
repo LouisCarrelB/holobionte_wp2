9ce0e415d5cb74bc8369e2b28ff0ebf147a0ef79
Louis Carrel <louis.carrel-billiard@inrae.fr>
2023-05-22 16:42:40 +0200

    parallel model

--- code/Modelisation_parallel.sh
+++ code/Modelisation_parallel.sh
@@ -1,27 +1,19 @@
 #!/bin/bash
 QSUB="qsub -S /usr/local/public/R/bin/Rscript -q short.q -cwd -V -M louis.carrel-billiard@inrae.fr -m a"
-render_script="code/Render_CV.R"
 Simulation_scenario=("microbiome" "join" "recursif")
-Modele="code/Modèle.R"
+Modele="/home/lcarrel/work/holobionte_wp2/code/Modèles.R"
 
-for {fold in 1.10}
+for fold in {1..10}
 do
-  for run in {21..25}
+  for run in {1..5}
   do
     for scenario in "${Simulation_scenario[@]}"
     do
-      $QSUB -N R_job_cr_fold_${scenario}_${run}_${fold} $Modele ${scenario} ${run} ${fold}
+      cd /home/lcarrel/work/holobionte_wp2/data/RDS/${scenario}/${run}/
+      $QSUB -N R_job_cr_fold_${scenario}_${run}_${fold} ${Modele} ${scenario} ${run} ${fold}
     done
   done
 done
 
 
-then 
 
-for run in {21..25}
-do
-  for scenario in "${Simulation_scenario[@]}"
-  do
-    $QSUB -N R_job_cr_modele_${scenario}_${run} $render_script ${scenario} ${run}
-  done
-done
